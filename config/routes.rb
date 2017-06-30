1c8ae447db26995ffd9fdfddbae27d8443f58d95
Khairul Annuar <khairulannuar.joroji@gmail.com>
2017-06-30 20:33:16 +0800

    session routes modified

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,14 @@
 Rails.application.routes.draw do
-  get 'sessions/new'
 
-  get 'sessions/create'
+  resources :sessions , only: [:new, :create, :destroy]
 
-  get 'sessions/destroy'
+  get 'signup', to: 'users#new', as: 'signup'
+  get 'login', to: 'sessions#new', as: 'login'
+  get 'logout', to: 'sessions#destroy', as: 'logout'
+
 
   resources :users
+
   get 'welcome/index'
   root 'welcome#index'
 
