93c8495f07d0cf285f3b064cb9281562a376e859
Khairul Annuar <khairulannuar.joroji@gmail.com>
2017-06-30 20:46:31 +0800

    sessions controller update

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -3,8 +3,16 @@ class SessionsController < ApplicationController
   end
 
   def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to root_url, notice: 'Logged in!'
+    else
+      render :new
   end
 
   def destroy
+    sessions[:user_id] = nil
+    redirect_to root_url, notice: 'Logged out!'
   end
 end
